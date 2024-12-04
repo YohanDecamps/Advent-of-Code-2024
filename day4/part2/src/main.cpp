dfc2aca1f1ad48c63dba9412293d51c65ce9bade
YohanDecamps <yohan.decamps@epitech.eu>
2024-12-04 10:52:55 +0100

    feat: clean day 4 part 2

--- day4/part2/src/main.cpp
+++ day4/part2/src/main.cpp
@@ -8,33 +8,24 @@
 
 #define FILE_PATH "../input.txt"
 
-
 int main() {
     std::vector<std::string> lines;
     std::ifstream file(FILE_PATH);
 
     for (std::string line; std::getline(file, line); lines.push_back(line));
 
-    int size_x = lines.at(0).size(), size_y = lines.size();
+    int size_x = lines.at(0).size();
+    int size_y = lines.size();
 
     int count = 0;
 
-    for (int i = 0; i < size_y - 2; i++) {
-        for (int j = 0; j < size_x - 2; j++) {
-            if (lines[i][j] == 'M' && lines[i][j + 2] == 'S' && lines[i + 1][j + 1] == 'A' && lines[i + 2][j] == 'M' && lines[i + 2][j + 2] == 'S') {
-                count++;
-            }
-            if (lines[i][j] == 'S' && lines[i][j + 2] == 'M' && lines[i + 1][j + 1] == 'A' && lines[i + 2][j] == 'S' && lines[i + 2][j + 2] == 'M') {
-                count++;
-            }
-            if (lines[i][j] == 'M' && lines[i][j + 2] == 'M' && lines[i + 1][j + 1] == 'A' && lines[i + 2][j] == 'S' && lines[i + 2][j + 2] == 'S') {
-                count++;
-            }
-            if (lines[i][j] == 'S' && lines[i][j + 2] == 'S' && lines[i + 1][j + 1] == 'A' && lines[i + 2][j] == 'M' && lines[i + 2][j + 2] == 'M') {
-                count++;
-            }
-        }
-    }
-    
+    for (int i = 0; i < size_y - 2; i++)
+        for (int j = 0; j < size_x - 2; j++)
+            count += ((lines[i][j] == 'M' && lines[i][j + 2] == 'S' && lines[i + 1][j + 1] == 'A' && lines[i + 2][j] == 'M' && lines[i + 2][j + 2] == 'S') ||
+            (lines[i][j] == 'S' && lines[i][j + 2] == 'M' && lines[i + 1][j + 1] == 'A' && lines[i + 2][j] == 'S' && lines[i + 2][j + 2] == 'M') ||
+            (lines[i][j] == 'M' && lines[i][j + 2] == 'M' && lines[i + 1][j + 1] == 'A' && lines[i + 2][j] == 'S' && lines[i + 2][j + 2] == 'S') ||
+            (lines[i][j] == 'S' && lines[i][j + 2] == 'S' && lines[i + 1][j + 1] == 'A' && lines[i + 2][j] == 'M' && lines[i + 2][j + 2] == 'M'));
+
+
     std::cout << count << std::endl;
 }
