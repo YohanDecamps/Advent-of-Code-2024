8777f4ffb030f628a03c9ec10d2d03eaf0d9c930
YohanDecamps <yohan.decamps@epitech.eu>
2024-12-05 09:52:56 +0100

    feat: day 4 part 1 & 2

---
+++ day5/part2/src/main.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#define FILE_PATH "../input.txt"
+
+int main() {
+    std::ifstream file(FILE_PATH);
+
+    std::unordered_map<int, std::vector<int>> rules;
+    for (std::string line; std::getline(file, line) && line.find('|') != std::string::npos;)
+        rules[std::stoi(line.substr(0, line.find('|')))].push_back(std::stoi(line.substr(line.find('|') + 1)));
+
+    std::vector<std::vector<int>> updates;
+    for (std::string line; std::getline(file, line);) {
+        std::vector<int> update;
+        std::stringstream ss(line);
+        for (std::string token; std::getline(ss, token, ',');)
+            update.push_back(std::stoi(token));
+        updates.push_back(update);
+    }
+
+    auto sortCondition = [&rules](int a, int b) {return std::find(rules[a].begin(), rules[a].end(), b) != rules[a].end();};
+
+    std::cout << std::accumulate(updates.begin(), updates.end(), 0,
+    [&sortCondition, &rules](int sum, std::vector<int> update) {
+        if (!std::is_sorted(update.begin(), update.end(), sortCondition)) {
+            std::nth_element(update.begin(), update.begin() + update.size() / 2, update.end(), sortCondition);
+            sum += update.at(update.size() / 2);
+        }
+        return sum;
+    }) << std::endl;
+}
