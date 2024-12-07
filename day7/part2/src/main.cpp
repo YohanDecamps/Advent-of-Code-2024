3511f6ef4428e2134fda3b65d6c3b5d900f079da
YohanDecamps <yohan.decamps@epitech.eu>
2024-12-07 10:05:52 +0100

    feat: day 7 part 1 & 2

---
+++ day7/part2/src/main.cpp
@@ -0,0 +1,63 @@
+#include <cmath>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#define FILE_PATH "../input.txt"
+
+enum class Operator {
+    PLUS,
+    MULTIPLY,
+    CONCATENATE
+};
+
+std::vector<Operator> generateOperators(std::vector<Operator> &operators) {
+    for (size_t i = 0; i < operators.size(); ++i) {
+        if (operators[i] == Operator::PLUS) {
+            operators[i] = Operator::MULTIPLY;
+            break;
+        } else if (operators[i] == Operator::MULTIPLY) {
+            operators[i] = Operator::CONCATENATE;
+            break;
+        } else {
+            operators[i] = Operator::PLUS;
+        }
+    }
+    return operators;
+}
+
+int main() {
+    std::ifstream file(FILE_PATH);
+
+    std::vector<std::pair<int64_t, std::vector<int64_t>>> nums;
+    for (std::string line; std::getline(file, line) && line.find(':') != std::string::npos;) {
+        std::stringstream ss(line.substr(line.find(':') + 1));
+        std::vector<int64_t> tempNums = {};
+        for (std::string num; ss >> num; tempNums.push_back(std::stoll(num)));
+        nums.push_back({std::stoll(line.substr(0, line.find(':'))), tempNums});
+    }
+
+    std::cout << std::accumulate(nums.begin(), nums.end(), 0LL, [&nums] (int64_t sum, std::pair<int64_t, std::vector<int64_t>> num) {
+        std::vector<Operator> operators(num.second.size() - 1, Operator::PLUS);
+        do {
+            int64_t tempValue = num.second[0];
+            for (size_t i = 0; i < operators.size(); i++)
+                if (operators[i] == Operator::PLUS)
+                    tempValue += num.second[i + 1];
+                else if (operators[i] == Operator::MULTIPLY)
+                    tempValue *= num.second[i + 1];
+                else
+                    tempValue = tempValue * (std::pow(10, std::to_string(num.second[i + 1]).size())) + num.second[i + 1];
+            if (tempValue == num.first)
+                return sum += num.first;
+            generateOperators(operators);
+        } while (!std::all_of(operators.begin(), operators.end(), [](Operator op) { return op == Operator::PLUS; }));
+        return sum;
+    }) << std::endl;
+
+}
